ac91fdd8e3c045aae3f142062249a90f0eadca14
Remon <feixiangxiaoshu@sina.com>
2018-10-21 20:31:05 +0800

    import db

--- app/src/main/java/com/example/admin/weatherdemo/MainActivity.java
+++ app/src/main/java/com/example/admin/weatherdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.admin.weatherdemo;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView cityTv,timeTv,humidityTv,weekTv,pmDataTv,pmQualityTv,
                                 temperatureTv,climateTv, windTv,city_name_Tv;
     private ImageView weatherImg,pmImg;
+    private  ImageView mCitySelect;
 
     private ImageView mUpdateBtn;
     private Handler mHandler=new Handler() {
@@ -41,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
                 default:
                     break;
+
             }
         }
     };
@@ -59,11 +62,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Log.d("myWeather","网络挂了");
             Toast.makeText(MainActivity.this,"网络挂了!",Toast.LENGTH_LONG).show();
         }
+        mCitySelect = (ImageView) findViewById(R.id.title_city_manager);
+        mCitySelect.setOnClickListener(this);
         initView();
 
     }
     @Override
     public void onClick(View view){
+        if (view.getId() == R.id.title_city_manager) {
+            Intent i = new Intent(this, SelectCity.class);
+            //startActivity(i);
+            startActivityForResult(i,1);
+        }
         if(view.getId()==R.id.title_update_btn){
             SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
             String cityCode = sharedPreferences.getString("main_city_code", "101010100");
@@ -79,6 +89,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
+
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(requestCode==1&&resultCode==RESULT_OK){
+            String newCityCode=data.getStringExtra("cityCode" );
+            Log.d("myWeather","选择的城市代码为"+newCityCode);
+
+            if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+                Log.d("myWeather","网络ok");
+                queryWeatherCode(newCityCode);
+            }else{
+                Log.d("myWeather","网络挂了");
+                Toast.makeText(MainActivity.this,"网络挂了!",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+
     public  void queryWeatherCode(String cityCode){
         final String address="http://wthrcdn.etouch.cn/WeatherApi?citykey="+cityCode;
         Log.d("myWeather",address);
@@ -193,10 +223,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             }
                         }
                         break;
-
-
-
-                    case XmlPullParser.END_TAG:
+                        case XmlPullParser.END_TAG:
                         break;
                 }
                 eventType = xmlPullParser.next();
